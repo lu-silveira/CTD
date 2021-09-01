2eab9342e94e659e1445e4215156a8bd200f274c
Lu Silveira <85649152+lu-silveira@users.noreply.github.com>
2021-09-01 12:45:19 -0300

    aula 11 micro exercicios

---
+++ ProgImperativa/aula-11/for-loop.js
@@ -0,0 +1,17 @@
+// 1. For loop que repita 5x
+
+for (let i = 0; i <= 4; i++) {
+    console.log(i);
+}
+
+// 2. Loop de números ímpares
+
+for (let i = 1; i <=10; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
+
+// 3. Criando a tabuada
+
+
---
+++ ProgImperativa/aula-11/while.js
@@ -0,0 +1,25 @@
+//While and do while
+
+//1. Números ímpares
+
+let impar = 0;
+while (impar<100) {
+    impar++;
+    if (impar % 2 !== 0) {
+        console.log(impar);       
+    } 
+}
+
+// 2. Números pares
+
+let pares = 0
+while (pares<=100) {
+    pares++;
+    if (pares % 2 === 0) {
+        console.log(pares);
+    }
+}
+
+//3. app números pares e ímpares
+
+//4. app média da sala
